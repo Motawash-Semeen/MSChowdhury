fc6794f9c065ad8da4ad3a779ebc6f726e13a1b6
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2023-02-17 19:49:46 +0600

    scroll-behaivor

--- ASSESTES/JS/custom.js
+++ ASSESTES/JS/custom.js
@@ -16,3 +16,25 @@ inputs.forEach((input) => {
     input.addEventListener("focus", focusFunc);
     input.addEventListener("blur", blurFunc);
 })
+
+
+
+// Get the button
+let mybutton = document.getElementById("top_button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
