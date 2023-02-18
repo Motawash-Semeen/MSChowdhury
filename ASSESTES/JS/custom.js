d3e9dc56d80479dac2e708d1a9882cad2e450c67
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2023-02-18 12:04:28 +0600

    swiper

--- ASSESTES/JS/custom.js
+++ ASSESTES/JS/custom.js
@@ -41,20 +41,16 @@ function topFunction() {
 
 
 
-let nav = document.querySelector('.nav')
 
-nav.querySelectorAll('li a').forEach((a, i) => {
-    a.onclick = (e) => {
-        if (a.classList.contains('nav-item-active')) return
-
-        nav.querySelectorAll('li a').forEach(el => {
-            el.classList.remove('nav-item-active')
-        })
-
-        a.classList.add('nav-item-active')
-
-        let nav_indicator = nav.querySelector('.nav-indicator')
-
-        nav_indicator.style.left = `calc(${(i * 120) + 60}px - 45px)`
-    }
-})
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  cssMode: true,
+  loop: true,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".next_testi",
+    prevEl: ".prev_testi",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
